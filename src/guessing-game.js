e2222d840d6819718160de69db096893ef5b581a
serezhishe <serezhishe@gmail.com>
2019-09-24 21:23:55 +0300

    feat: task done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min + 1;
+        this.max = max - 1;
     }
 
     guess() {
-
+        this.current = Math.ceil((this.max + this.min)/2);
+        return this.current;
     }
 
     lower() {
-
+        this.max = this.current - 1;
     }
 
     greater() {
-
+        this.min = this.current + 1;
     }
 }
 
